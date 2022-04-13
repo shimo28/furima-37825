1c29acaff7db3dc2d989503c526e0619615992a7
shimo28 <shun20000816@gmail.com>
2022-04-13 18:39:09 +0900

    モデルの単体テストコード（修正）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,6 +79,11 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include('Password は半角英数を両方含む必要があります')
       end
+      it 'passwordに全角文字を含む場合は登録できない' do
+        @user.password = '１２３４５６'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password は半角英数を両方含む必要があります')
+      end
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = 'abc123'
         @user.password_confirmation = 'abc1234'
