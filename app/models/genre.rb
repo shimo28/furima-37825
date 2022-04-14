9f0492a9eb883b7419cf887424005e3d76225247
shimo28 <shun20000816@gmail.com>
2022-04-14 12:34:26 +0900

    バリデーション

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,8 @@
 class Genre < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+  ]
+
   include ActiveHash::Associations
   has_many :items
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+
+  validates :title,       presence: true
+  validates :explanation, presence: true
+  validates :price,       presence: true
+  validates :category_id,        numericality: { other_than: 1 }
+  validates :condition_id,       numericality: { other_than: 1 }
+  validates :prefecture_id,      numericality: { other_than: 1 }
+  validates :shipping_burden_id, numericality: { other_than: 1 }
+  validates :delivery_day_id,    numericality: { other_than: 1 }
 end
