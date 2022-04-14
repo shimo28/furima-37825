6ac0cc3a83b6cceed98161b815ec116c91bcd093
shimo28 <shun20000816@gmail.com>
2022-04-14 13:25:57 +0900

    商品出品ページを表示
    imageのバリテーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   validates :title,       presence: true
   validates :explanation, presence: true
   validates :price,       presence: true
+  validates :image,       presence: true
   validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id,       numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,      numericality: { other_than: 1 , message: "can't be blank"}
