c777fa9df3bb1d88551637bcb3006d2e8879cea3
shimo28 <shun20000816@gmail.com>
2022-04-14 17:26:37 +0900

    バリテーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
 
   validates :title,       presence: true
   validates :explanation, presence: true
-  validates :price,       presence: true
+  validates :price,       presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :image,       presence: true
   validates :category_id,        numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id,       numericality: { other_than: 1 , message: "can't be blank"}
