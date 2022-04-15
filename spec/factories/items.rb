8266acd07624948840eadfd1af0f385b40099959
shimo28 <shun20000816@gmail.com>
2022-04-15 10:45:03 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    title {'test'}
+    explanation {'test'}
+    price {2000} 
+    category_id {2}
+    condition_id {2}
+    prefecture_id {2}
+    shipping_burden_id {2}
+    delivery_day_id {2}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
